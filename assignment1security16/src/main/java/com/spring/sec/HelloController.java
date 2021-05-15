ef8bc53b72435ce96eaa55e07e536ce6412c1cd4
borsepooja007 <59304709+borsepooja007@users.noreply.github.com>
2021-05-15 11:57:47 +0530

    Add files via upload

---
+++ assignment1security16/src/main/java/com/spring/sec/HelloController.java
@@ -0,0 +1,17 @@
+package com.spring.sec;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	
+	@GetMapping("/admin")
+	public String admin()
+	{	
+		return "Welcome Admin";
+	}
+	
+
+}
